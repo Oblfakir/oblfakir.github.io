faceeeb88cc071895f748b37c3e370d04b861ad4
Mikhail Drobynin <mdrobynin@griddynamics.net>
2020-01-10 16:37:27 +0400

    123

---
+++ precache-manifest.faa16166afc912d78db6c9c326db6033.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd2c9290e06ce83f173536bcff321b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d455e24ce4191f00ba3d",
+    "url": "/static/css/main.f203232b.chunk.css"
+  },
+  {
+    "revision": "1b4b02892740bf8e9409",
+    "url": "/static/js/2.f97aede0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f97aede0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d455e24ce4191f00ba3d",
+    "url": "/static/js/main.aea45e59.chunk.js"
+  },
+  {
+    "revision": "371edd36059d2d232f6f",
+    "url": "/static/js/runtime-main.d26df5f4.js"
+  }
+]);
